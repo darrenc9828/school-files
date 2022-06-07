c537467fb1fcfabaa9733ce5ab13b0312b678d95
Darren Caldwell <darrenc9828@gmail.com>
2022-06-07 18:31:03 -0400

    changes on 6/7

--- classesseries.py
+++ classesseries.py
@@ -3,6 +3,7 @@
 from calendar import weekday
 
 
+
 class Employee:
     num_of_emps = 0
     raise_amount = 1.04
@@ -23,27 +24,36 @@ class Employee:
     def set_raise_amount(cls, amount):
          cls.raise_amount = amount
          
-    @classmethod
-    def from_string(cls, emp_str):
-         first, last, pay =emp_str.split('-')
-         return cls(first, last, pay)
+class Developer(Employee):
+       pass
+    
+dev_1 = Developer('Corey', 'Schafer', 50000)
+dev_2 = Developer('Test', 'Employee', 60000)
+
+#print(dev_1.email)
+#print(dev_2.email)
+       
+  # @classmethod
+   # def from_string(cls, emp_str):
+      #   first, last, pay =emp_str.split('-')
+       #  return cls(first, last, pay)
          
-    @staticmethod
-    def is_workday(day):
-        if day.weekday() == 5 or day.weekday() ==6:
-           return False   
-        return True
+    #@staticmethod
+    #def is_workday(day):
+     #   if day.weekday() == 5 or day.weekday() ==6:
+      #     return False   
+       # return True
      
-Employee.set_raise_amount(1.05)  
+#Employee.set_raise_amount(1.05)  
          
-emp_1 = Employee('Corey', 'Schafer', 50000)
-emp_2 = Employee('Test' , 'Ueser' , 60000)
+#emp_1 = Employee('Corey', 'Schafer', 50000)
+#emp_2 = Employee('Test' , 'Ueser' , 60000)
 
 
 
-import datetime
-my_date = datetime.date(2016, 7, 11)
+#import datetime
+#my_date = datetime.date(2016, 7, 11)
 
-print(Employee.is_workday(my_date))
+#print(Employee.is_workday(my_date))
 
 #test to see if it changes.
